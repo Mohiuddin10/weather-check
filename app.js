0f77682db7e336c71fbe30031cb3b9eab652c8fc
Mohiuddin10 <sj.ctg6737@gmail.com>
2021-12-09 01:27:20 +0600

    secound commite

---
+++ app.js
@@ -0,0 +1,29 @@
+const apiKey = `46ad7457603b9b0104e633e78cd60e16`;
+
+const searchTemp = () => {
+    const city = document.getElementById("city-name");
+    const cityName = city.value;
+    city.value = '';
+
+
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+    console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayTemp(data))
+}
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
+const displayTemp = temp => {
+    setInnerText('city', temp.name);
+    setInnerText("temp", temp.main.temp);
+    setInnerText("description", temp.weather[0].main);
+    // set weather icon 
+    const url = `https://openweathermap.org/img/wn/${temp.weather[0].icon}@2x.png`;
+    const image = document.getElementById('icon');
+    image.setAttribute('src', url);
+
+}
